a1103e3177a673289b67750aaf15bb78a5838e25
Rustam Osmanov <rustam-osmanov@yandex.ru>
2026-05-28 21:22:03 +0300

    MVC + HIBERNATE + AOP + REST API

--- src/main/java/ru/rustamosmanov/spring/mvc/restapi/controller/MyRESTController.java
+++ src/main/java/ru/rustamosmanov/spring/mvc/restapi/controller/MyRESTController.java
@@ -1,8 +1,22 @@
 package ru.rustamosmanov.spring.mvc.restapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.rustamosmanov.spring.mvc.entity.EmployeeBD;
+import ru.rustamosmanov.spring.mvc.service.EmployeeService;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MyRESTController {
+    @Autowired
+    public EmployeeService employeeService;
 
+    @GetMapping("/employees")
+    public List<EmployeeBD> showAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
 }
